98c24096dea90b5440b5feba5a44bc05c2ab6c09
Gordon DuQuesnay <gduquesnay@gmail.com>
2017-08-16 06:26:56 +0000

    initial commit

--- bot.rb
+++ bot.rb
@@ -1,27 +1,15 @@
 #!/usr/bin/env ruby
 #
 #
-PROMPT_1 = ENV['PROMPT_1'] || 'Default prompt 1'
-PROMPT_2 = ENV['PROMPT_2'] || 'Default prompt 2'
-PROMPT_3 = ENV['PROMPT_3'] || 'Default prompt 3'
-SIGNATURE = ENV['SIGNATURE'] || 'Default signature prompt'
+
+PATTERN = Regexp.new (ENV['PATTERN'] || '')
+COMMAND = ENV['COMMAND'] || ''
 
 
 require "./lib/greenbot.rb"
-tell PROMPT_1
-issue = note(PROMPT_2)
-if confirm("Would you like someone to contact you?")
-  contact_me = true
-  contact_me.remember("contact_me")
-  name = ask("When we call, who should we ask for?")
-  name.remember("who_to_ask_for")
-  if confirm("Is there another number we should try?")
-    better_number = ask("Please enter that number with an area code")
-    better_number.remember("better_number")
+while true
+  message = gets
+  if message =~ PATTERN
+    tell COMMAND
   end
-else
-  tell("No problem at all.")
 end
-tell PROMPT_3
-tell SIGNATURE
-
